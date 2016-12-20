2ebf8b56a15d4d9f255136d58ae62c97aa2b734e
jason brown <j.grant.brown@gmail.com>
2016-12-20 18:52:16 +0000

    Done.

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,10 @@
 
 def levitation_quiz
-	#your code here
+puts "What is the spell that enacts levitation?"
+  answer=gets.chomp
+  if answer=="Wingardium Leviosa"
+    puts "You passed the quiz!"
+
+  #your code here
 end
-
-
+end
